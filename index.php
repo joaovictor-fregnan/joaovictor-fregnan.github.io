7527989ad25c46c53ebf62aba37396e309fda643
João Victor Fregnan <joaovictorlf10@outlook.com>
2020-10-17 19:53:01 -0300

    Segundo commit

--- index.php
+++ index.php
@@ -7,11 +7,123 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Pokédex</title>
+        <style>
+            html, body
+            {                
+                font-size: 4px;
+                min-height: 100%;
+            }
+
+            body
+            {            
+                padding: 0;
+                margin: 0;                
+                font-family: Calibri;
+                background-color: rgb(51, 51, 51);                
+            }
+            
+            #content
+            {
+                width: 1024px;
+                background-color: white;                
+                margin: auto;
+                padding: 5% 0 5% 0;
+                text-align: center;
+            }
+            
+            .pokemon
+            {
+                display: inline-table;
+                width: 20%;   
+                text-align: left;
+                padding: 0 1% 8% 1%;
+            }
+                                    
+            .pokemon img
+            {
+                width: 100%;
+                background-color: rgb(235, 235, 235);
+                border-radius: 1rem;
+            }
+            
+            .pokemon p
+            {
+                margin: 0 0 4% 0;
+                font-size: 4rem;
+                color: rgb(128, 128, 128);
+                text-align: left;
+                font-weight: bold;
+            }
+            
+            .pokemon h2
+            {
+                font-size: 8rem;
+                color: rgb(51, 51, 51);
+                margin: 0 0 2% 0;
+                text-align: left;
+            }
+            
+            .pokemon-type
+            {
+                width: 20%;                
+                background-color: green;
+                font-size: 3rem;
+                padding: 1% 8% 1% 8%;
+                border-radius: 4px;
+                line-height: 1;
+                vertical-align: middle;
+                display: inline-block;
+                margin: 0 2% 0 0;  
+                text-align: center;
+            }
+            
+        </style>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div id="content">
+            <div class="pokemon">
+                <img src="img/bulbasaur.png" alt="">
+                <p>Nº001</p>
+                <h2>Bulbasaur</h2>
+                <div class="pokemon-type">Grass</div>
+                <div class="pokemon-type">Poison</div>
+            </div>
+            <div class="pokemon">
+                <img src="img/bulbasaur.png" alt="">
+                <p>Nº001</p>
+                <h2>Bulbasaur</h2>
+                <div class="pokemon-type">Grass</div>
+                <div class="pokemon-type">Poison</div>
+            </div>
+            <div class="pokemon">
+                <img src="img/bulbasaur.png" alt="">
+                <p>Nº001</p>
+                <h2>Bulbasaur</h2>
+                <div class="pokemon-type">Grass</div>
+                <div class="pokemon-type">Poison</div>
+            </div>
+            <div class="pokemon">
+                <img src="img/bulbasaur.png" alt="">
+                <p>Nº001</p>
+                <h2>Bulbasaur</h2>
+                <div class="pokemon-type">Grass</div>
+                <div class="pokemon-type">Poison</div>
+            </div>
+            <div class="pokemon">
+                <img src="img/bulbasaur.png" alt="">
+                <p>Nº001</p>
+                <h2>Bulbasaur</h2>
+                <div class="pokemon-type">Grass</div>
+                <div class="pokemon-type">Poison</div>
+            </div>
+            <div class="pokemon">
+                <img src="img/bulbasaur.png" alt="">
+                <p>Nº001</p>
+                <h2>Bulbasaur</h2>
+                <div class="pokemon-type">Grass</div>
+                <div class="pokemon-type">Poison</div>
+            </div>
+        </div>
     </body>
 </html>
--- models/pokemonDAO.php
+++ models/pokemonDAO.php
@@ -1,6 +1,9 @@
 <?php
 
 require_once 'Pokemon.php';
+require '../vendor/autoload.php';
+
+use PokePHP\PokeApi;
 
 class PokemonDAO
 {
@@ -11,8 +14,36 @@ class PokemonDAO
         echo print_r($xml->pokemon[1]);
         echo '</pre>';
     } 
+    
+    function getNext10($start_id)
+    {
+        $api = new PokeApi;        
+        $pokemons = array();
+        
+        for($i=$start_id; $i<$start_id + 10; $i++)
+        {            
+            $pokemons[] = json_decode($api->pokemon($i));     
+        }        
+        return $pokemons;
+    }
 }
 
-$pokemonDAO = new PokemonDAO();
+$api = new PokeApi;   
+echo '<pre>';
+echo print_r(json_decode($api->pokemon(1)));
+echo '</pre>';
 
-$pokemonDAO->readXML();
+/*$pokemonDAO = new PokemonDAO();
+$pokemons = $pokemonDAO->getNext10(10);
+
+foreach($pokemons as $pokemon)
+{
+    echo "===== ".$pokemon->name." =====<br>";
+    $types = $pokemon->types;
+    foreach ($types as $type)
+    {
+        echo $type->type->name."<br>";
+    }
+    echo "<br><br>";
+}*/
+
